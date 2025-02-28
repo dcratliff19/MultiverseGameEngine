7a9cd1798f65b62901b583f9f21be8de359f015c
MrSully97 <csully710@gmail.com>
2025-02-27 20:00:37 -0500

    tribe network model

--- src/networking/PeerManager.js
+++ src/networking/PeerManager.js
@@ -1,30 +1,63 @@
 import Peer from "peerjs";
 import { PhysicsObject } from "../components/PhysicsObject";
+import { EventStreamManager } from "./streams/EventStreamManager";
 
 export class PeerManager {
     constructor(game) {
         this.game = game;
         this.peer = null;
         this.connections = [];
+        this.isHost = false;
         this.isMultiplayer = false;
+        this.streamManagers = {
+            event: new EventStreamManager(this),
+            // ghost: new GhostStreamManager(this),
+            // move: new MoveStreamManager(this),
+            // datablock: new DatablockStreamManager(this),
+            // string: new StringStreamManager(this)
+        };
         this.setupSync();
     }
 
     startAsHost() {
-        this.peer = new Peer();
+        this.isHost = true;
+        this.peer = new Peer({
+            config: {
+                iceServers: [
+                    { urls: "stun:stun.l.google.com:19302" },
+                    { 
+                        urls: "turn:openrelay.metered.ca:80",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    }
+                ]
+            }
+        });
         this.peer.on("open", (id) => {
             document.getElementById("myId").textContent = id;
             this.isMultiplayer = true;
         });
         this.peer.on("connection", (conn) => {
             this.connections.push(conn);
-            conn.on("data", (data) => this.handleData(data));
+            conn.on("data", (data) => this.receiveData(data));
             this.sendFullState(conn);
         });
     }
 
     join(peerId) {
-        this.peer = new Peer();
+        this.isHost = false;
+        this.peer = new Peer({
+            config: {
+                iceServers: [
+                    { urls: "stun:stun.l.google.com:19302" },
+                    { 
+                        urls: "turn:openrelay.metered.ca:80",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    }
+                ]
+            }
+        });
         this.peer.on("open", (myId) => {
             document.getElementById("myId").textContent = myId;
             const conn = this.peer.connect(peerId);
@@ -32,38 +65,69 @@ export class PeerManager {
                 this.connections.push(conn);
                 this.isMultiplayer = true;
             });
-            conn.on("data", (data) => this.handleData(data));
+            conn.on("data", (data) => this.receiveData(data));
         });
     }
 
     setupSync() {
         this.game.scene.onBeforeRenderObservable.add(() => {
-            if (this.isMultiplayer && this.connections.length > 0) {
+            if (this.isMultiplayer && this.connections.length > 0 && this.isHost) {
                 const state = {
                     id: this.peer.id,
                     position: this.game.player.mesh.position.asArray(),
                     velocity: this.game.player.physicsBody.getLinearVelocity().asArray()
                 };
-                this.connections.forEach((conn) => conn.send(state));
+                this.sendDataToPeers({ streamType: "ghost", payload: state });
             }
         });
     }
 
     sendFullState(conn) {
         const state = this.game.getState();
-        conn.send({ type: "fullSync", state });
+        conn.send({ streamType: "datablock", payload: state });
+    }
+
+    receiveData(data) {
+        const { streamType, payload } = data;
+        if (this.streamManagers[streamType]) {
+            this.streamManagers[streamType].handleIncomingData(payload);
+        } else if (streamType === "ghost") {
+            // Handle ghost updates directly for now
+            if (!this.game.remotePlayers[payload.id]) {
+                this.game.remotePlayers[payload.id] = new PhysicsObject(`player-${payload.id}`, this.game.scene, this.game.physicsPlugin);
+            }
+            const player = this.game.remotePlayers[payload.id];
+            player.mesh.position.set(...payload.position);
+            player.physicsBody.setLinearVelocity(new BABYLON.Vector3(...payload.velocity));
+        } else if (streamType === "datablock") {
+            // Handle full state sync for joining peers
+            this.applyFullState(payload);
+        }
+    }
+
+    sendDataToPeers(data) {
+        if (this.isHost) {
+            this.connections.forEach(conn => conn.send(data));
+        } else if (this.connections.length) {
+            this.connections[0].send(data); // Send to host
+        }
+    }
+
+    applyFullState(state) {
+        this.game.player.mesh.position.set(...state.player.position);
+        this.game.player.physicsBody.setLinearVelocity(new BABYLON.Vector3(...state.player.velocity));
+        state.objects.forEach((obj) => {
+            let mesh = this.game.scene.getMeshByName(obj.name);
+            if (!mesh) {
+                mesh = new PhysicsObject(obj.name, this.game.scene, this.game.physicsPlugin);
+            }
+            mesh.mesh.position.set(...obj.position);
+            mesh.physicsBody.setLinearVelocity(new BABYLON.Vector3(...obj.velocity));
+        });
     }
 
     handleData(data) {
-        if (data.type === "fullSync") {
-            this.game.stateManager.applyFullState(data.state);
-        } else {
-            if (!this.game.remotePlayers[data.id]) {
-                this.game.remotePlayers[data.id] = new PhysicsObject(`player-${data.id}`, this.game.scene, this.game.physicsPlugin);
-            }
-            const player = this.game.remotePlayers[data.id];
-            player.mesh.position.set(...data.position);
-            player.physicsBody.setLinearVelocity(new BABYLON.Vector3(...data.velocity));
-        }
+        // Legacy method, redirect to receiveData
+        this.receiveData(data);
     }
 }
